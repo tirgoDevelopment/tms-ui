635a35447c430e573e624e6ea387620efb752b87
shonasreeetdinov <najmiddinshonasriddinov@gmail.com>
2024-03-12 17:06:22 +0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const env = {
   production: true,
-  apiUrl: 'http://65.1.134.189/api/v2',
+  apiUrl: 'https://test-api.tirgo.io/api/v2',
+  apiUsers: 'https://test-api.tirgo.io/api/v2',
+  apiOrders: 'https://test-api.tirgo.io/api/v2',
+  apiReferences: 'https://test-api.tirgo.io/api/v2',
+  apiFinance: 'https://test-api.tirgo.io/api/v2',
 };
